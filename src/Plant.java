cfadafcc07bcea87789fbefef718d85360ac0a9b
Joe Forte <josephforte05@gmail.com>
2024-02-21 19:34:03 -0500

    Plant toString and beginning of genus and species validation

--- src/Plant.java
+++ src/Plant.java
@@ -1,7 +1,66 @@
 import java.time.LocalDate;
 import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Plant {
+    //TODO: Verify if this function should halt execution or simply alert
+    //  If the intended behaviour is that the program will throw
+    //  an error if given an invalid 'String' then 'throw' statements
+    //  should be added. If it should only alert the user there is an
+    //  error then 'System.err.println' should be used.
+    ///////////////////////////////////////////////////
+    // This implementation is only alerting the user //
+    ///////////////////////////////////////////////////
+    private static void validateGenusSpecies(String genusSpecies){
+        if (genusSpecies.length() < 7 || genusSpecies.length() > 39){
+            //Breaks character bounds defined in Canvas
+            System.err.println("Invalid plant name: " + genusSpecies);
+            System.err.println("Breaks bounds. Length: " + genusSpecies.length());
+            
+            //Because this 'if' statement only runs if the name
+            //  is invalid, there is no point in checking the other
+            //  conditions. So we can return here.
+            return;
+            //This 'early return' will be used in all subsequent checks
+        }
+        if (genusSpecies.split(" ").length == 1){
+            //Is only one word
+            System.err.println("Invalid plant name: " + genusSpecies);
+            System.err.println("The name is only one word");
+
+            return; //Early return
+        }
+
+        //Creates a 'Matcher' object which will match only the uppercase
+        //  letters in the plant name.
+        Matcher uppercaseMatcher = Pattern.compile("[A-Z]").matcher(genusSpecies);
+        //Uses the static method 'Pattern.compile' to create
+        //  the RegEx to use on the 'String'. The 'matcher' method
+        //  creates the actual 'Matcher' object which will be used to
+        //  determine how many and where the uppercase letters are. 
+
+
+        if (uppercaseMatcher.find()){
+            //Has uppercase letters
+            
+            if (Character.isLowerCase(genusSpecies.charAt(0))){
+                //First letter of genus is lowercase
+                System.err.println("Invalid plant name: " + genusSpecies);
+                System.err.println("The first letter of the genus should be the only uppercase letter");
+
+                return; //Early return
+            }
+            
+            //TODO: Add if statment for multiple uppercase letters
+
+        }
+        else{
+            //Does not contain any uppercase letters
+            System.err.println("Invalid plant name: " + genusSpecies);
+            System.err.println("No capital letters. The first letter of the genus should be uppercase");
+        }
+    }
 
     private long id;
     private String genusSpecies;
@@ -16,5 +75,17 @@ public class Plant {
         this.genusSpecies = genusSpecies;
         this.commonName = commonName;
         this.plantGroup = plantGroup;
+    }
+
+    public boolean growsInZone(int zoneNumber){
+        //TODO: Double check this is the intended behaviour
+        //  Assumes that the zones 'HashMap' will only have the zones that
+        //  the plant is able to grow in. 
+        return zones.keySet().contains(zoneNumber);
+    }
+
+    @Override
+    public String toString(){
+        return commonName + " (" + genusSpecies.split(" ")[0] + ")";
     }
 }
